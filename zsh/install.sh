66b1ff69e3f8dc25a709487f43c711e3027cb4b1
Lyuzhi Pan <panlvzhi@foxmail.com>
2021-01-06 14:36:13 +0800

    Update install.sh

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+# zsh-autosuggestions
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
+#zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+#git-open
+git clone https://github.com/paulirish/git-open.git $ZSH_CUSTOM/plugins/git-open
